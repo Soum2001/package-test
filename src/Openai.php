b5e04fe14d3eca9a4d3f7a595ae48ac92f087299
soumya <soumya.samantaray@mindfiresolutions.com>
2023-01-25 15:04:47 +0530

    created contructer function

--- src/Openai.php
+++ src/Openai.php
@@ -4,126 +4,121 @@ namespace Soumyaa\Openai;
 
 class Openai
 {
-    public function completion($api)
-    {
-        $apiKey = $api;
-        $data = [
-            'prompt' => 'What is the capital of odisha',
-        ];
-        $url = "https://api.openai.com/v1/engines/text-davinci-002/completions";
-        $type = "POST";
-        return $this->callOpenAI($data, $url, $type, $apiKey);
-    }
-    public function fineTune($api)
-    {
-        $apiKey = $api;
-        $data = [
-            "model" => "curie",
-            "prompt" => "Overjoyed with the new iPhone! ->",
-            //"completion"=>" positive",
-            "max_tokens" => 1,
+    private string $content_types;
+    private string $url;
+    private string $openai_key;
 
-        ];
-        $url = "https://api.openai.com/v1/completions";
-        $type = "POST";
-        return $this->callOpenAI($data, $url, $type, $apiKey);
-    }
-    public function edit($api)
+    public function __construct($openai_key,$url,$content_types)
     {
-        $apiKey = $api;
-        $data = [
-            "model" => "text-davinci-edit-001",
-            "input" => "What day of the wek is it?",
-            "instruction" => "Fix the spelling mistakes",
-        ];
-        $url = "https://api.openai.com/v1/edits";
-        $type = "POST";
-        return $this->callOpenAI($data, $url, $type, $apiKey);
-    }
-    public function embeddings($api)
-    {
-        $apiKey = $api;
-        $data = [
-            "model" => "text-embedding-ada-002",
-            "input" => "The food was delicious and the waiter...",
-            "instruction" => "Fix the spelling mistakes",
-        ];
-        $url = "https://api.openai.com/v1/embeddings";
-        $type = "POST";
-        return $this->callOpenAI($data, $url, $type, $apiKey);
-    }
-    public function moderation($api)
-    {
-        $apiKey = $api;
-        $data = [
-            "input" => "I want to kill them.",
-        ];
-
-        $url = "https://api.openai.com/v1/moderations";
-        $type = "POST";
-        return $this->callOpenAI($data, $url, $type, $apiKey);
-    }
-    public function engines($api)
-    {
-        $apiKey = $api;
-        $data = [];
-        $url = "https://api.openai.com/v1/engines";
-        $type = "GET";
-        return $this->callOpenAI($data, $url, $type, $apiKey);
-    }
-
-    public function callOpenAI($data, $url, $type, $apiKey)
-    {
-        if ($type == "POST") {
-            $url = $url;
-            $prompt = "What is the capital of odisha";
-
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $url,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => $type,
-                CURLOPT_POSTFIELDS => json_encode($data),
-                CURLOPT_HTTPHEADER => array(
-                    "Content-Type: application/json",
-                    "Authorization: Bearer $apiKey"
-                ),
-            ));
-
-            $response = curl_exec($curl);
-            curl_close($curl);
-            $response_data = json_decode($response, true);
-            print_r($response_data);
-            $embeddings = $response_data['choices'][0]['text'];
-            return $embeddings;
-        } else {
-            $url = $url;
-            $curl = curl_init();
-            curl_setopt_array($curl, array(
-                CURLOPT_URL => $url,
-                CURLOPT_RETURNTRANSFER => true,
-                CURLOPT_ENCODING => "",
-                CURLOPT_MAXREDIRS => 10,
-                CURLOPT_TIMEOUT => 0,
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-                CURLOPT_CUSTOMREQUEST => $type,
-                CURLOPT_HTTPHEADER => array(
-                    "Content-Type: application/json",
-                    "Authorization: Bearer $apiKey"
-                ),
-            ));
-            $response = curl_exec($curl);
-            curl_close($curl);
-            $response_data = json_decode($response, true);
-            print_r($response_data);
-            $embeddings = $response_data['choices'][0]['text'];
-            return $embeddings;
+        $this->openai_key = $openai_key;
+        $this->url = "https://api.openai.com/v1/".$url;
+        if($content_types == "file/image")
+        {
+            $this->content_types = "multipart/form-data";
         }
+        else{
+            $this->content_types = "application/json";
+        }
+    }
+    public function listModels()
+    {
+        $parameter = array(
+            'type' => "GET",
+            'url' => $this->url,
+        );
+        return $this->callOpenAI($parameter);
+    }
+    public function retriveModels()
+    {
+        $parameter = array(
+            'type' => "GET",
+            'url' => $this->url,
+        );
+        return $this->callOpenAI($parameter);
+    }
+    public function completion($data)
+    {
+        $parameter = array(
+            'data' => array(
+                "model" => $data['model'],
+                'prompt' => $data['prompt'],
+                "max_tokens" => 7,
+                "temperature" => 0,
+            ),
+            'type' => "POST",
+        );
+        return $this->callOpenAI($parameter);
+    }
+    public function imageGenerations($data)
+    {
+        $parameter = array(
+            'data' => array(
+                "prompt" => $data['prompt'],
+                "n" => $data['n'],
+                "size" => $data['size'],
+            ),
+            'type' => "POST",
+        );
+        return $this->callOpenAI($parameter);
+    }
+
+    public function imageEdit($data)
+    {
+        $parameter = array(
+            'data' => json_encode(array(
+                "prompt" => $data['prompt'],
+                "n" => $data['n'],
+                "size" => $data['size'],
+            )),
+            'type' => "POST",
+        );
+        return $this->callOpenAI($parameter);
+    }
+
+    public function imageVariations($data)
+    {
+        $parameter = array(
+            'api_key' => "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",
+            'data' => array(
+                "prompt" => $data['prompt'],
+                "n" => $data['n'],
+                "size" => $data['size'],
+            ),
+            'type' => "POST",
+        );
+        return $this->callOpenAI($parameter);
+    }
+    public function callOpenAI($parameter)
+    {
+        if ($parameter["data"]["prompt"] == "") {
+            return "Give some message in prompt";
+        }
+        $url = $parameter['url'];
+        //$prompt ="What is the capital of odisha";
+        $curl = curl_init();
+        if ($parameter['type'] == "POST" && $this->content_types = "application/json") {
+            curl_setopt_array($curl, array(CURLOPT_POSTFIELDS => json_encode($parameter['data'])));
+        }
+        elseif ($parameter['type'] == "POST" && $this->content_types = "multipart/form-data") {
+            curl_setopt_array($curl, array(CURLOPT_POSTFIELDS => $parameter['data']));
+        }
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => $url,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $parameter['type'],
+            CURLOPT_HTTPHEADER => array(
+                "Content-Type:".$this->content_types,
+                "Authorization: Bearer " . $this->openai_key
+            ),
+        ));
+        $response = curl_exec($curl);
+        curl_close($curl);
+        $response_data = json_decode($response, true);
+        return $response_data;
     }
 }
